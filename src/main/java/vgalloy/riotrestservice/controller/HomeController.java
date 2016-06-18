dc815e84fe4ed532fe38600f538b916168691aa5
Vincent Galloy <vgalloy@excilys.com>
2016-06-18 09:45:44 +0200

    [ FIX ] : Checkstyle

--- src/main/java/vgalloy/riotrestservice/controller/HomeController.java
+++ src/main/java/vgalloy/riotrestservice/controller/HomeController.java
@@ -2,8 +2,14 @@ package vgalloy.riotrestservice.controller;
 
 /**
  * @author Vincent Galloy
- * Created by Vincent Galloy on 18/06/16.
+ *         Created by Vincent Galloy on 18/06/16.
  */
 public interface HomeController {
+
+    /**
+     * Get the home page.
+     *
+     * @return Some information
+     */
     String getHome();
 }
